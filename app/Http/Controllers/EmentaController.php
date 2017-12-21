91bae1d3894abedf091e79de6518797dd982b1d9
siravaf <32363129+siravaf@users.noreply.github.com>
2017-12-21 20:14:26 +0000

    help rafa

--- app/Http/Controllers/EmentaController.php
+++ app/Http/Controllers/EmentaController.php
@@ -25,4 +25,30 @@ class EmentasController extends Controller
     {
         return response()->json(Ementa::getEmentasDetails($id));
     }
+
+    public function delete($id) {
+        DB::table('ementas')->where('id','==',$id)->delete();
+    }
+
+    public function update(Request $request, $id) {
+        DB::table('ementas')
+            ->where('id', $id)
+            ->update(['ementa' => 1, 'preco' => 0]);
+
+
+
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+        ]);
+        if ($validator->fails()) {
+            return redirect()->back()->withErrors($validator)->withInput();
+        }
+        $customer = $this->customer->query()->find($id);
+        $customer->name = $request->input('name');
+        $customer->phone = $request->input('phone');
+        $customer->address = $request->input('address');
+        $customer->save();
+        return redirect()->route('customer.index');
+    }
 }
